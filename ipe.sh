7fc6ef18d1041f2cc792c0defaf3c404b54a5aa1
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2018-07-09 15:41:13 +0700

    Move stylesheets to subdirectory

--- ipe.sh
+++ ipe.sh
@@ -4,8 +4,10 @@ THIS_DIR="$(dirname "$(realpath "$0")")"
 
 # IPE Configuration
 # See more: http://ipe.otfried.org/manual/manual_40.html
-export EDITOR=atom
-export IPELATEXDIR="$THIS_DIR/build"
+export IPELATEXDIR="${IPELATEXDIR:-"$THIS_DIR/build"}"
+export EDITOR="${EDITOR:-/usr/bin/atom}"
+export IPESTYLES="_:/home/kite/.ipe/styles:$THIS_DIR/styles"
+export IPELETPATH="_:/home/kite/.ipe/ipelets:$THIS_DIR/ipelets"
 
 # Run IPE located in $PATH
 ipe
