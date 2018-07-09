0efb6b66c9f7e44a6c9e55951f02a9eaec3c6f16
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2018-07-10 00:17:46 +0700

    Add Thai fonts setup

--- ipe.sh
+++ ipe.sh
@@ -2,12 +2,21 @@
 # Directory containing this script
 THIS_DIR="$(dirname "$(realpath "$0")")"
 
+# Related directories
+BUILD_DIR="$THIS_DIR/build"
+TEXPACKAGE_DIR="$THIS_DIR/texpackage"
+IPEHOME_DIR="$HOME/.ipe"
+
+# Copy necessary LaTeX-related files into build directory
+mkdir -p "$BUILD_DIR"
+cp -r "$TEXPACKAGE_DIR/." "$BUILD_DIR"
+
 # IPE Configuration
 # See more: http://ipe.otfried.org/manual/manual_40.html
-export IPELATEXDIR="${IPELATEXDIR:-"$THIS_DIR/build"}"
+export IPELATEXDIR="${IPELATEXDIR:-"$BUILD_DIR"}"
 export EDITOR="${EDITOR:-/usr/bin/atom}"
-export IPESTYLES="_:/home/kite/.ipe/styles:$THIS_DIR/styles"
-export IPELETPATH="_:/home/kite/.ipe/ipelets:$THIS_DIR/ipelets"
+export IPESTYLES="_:$IPEHOME_DIR/styles:$THIS_DIR/styles"
+export IPELETPATH="_:$IPEHOME_DIR/ipelets:$THIS_DIR/ipelets"
 
 # Run IPE located in $PATH
 ipe
